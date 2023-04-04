47c980d2f338b3df439b6b5264a048d1ba9911b0
DaviCoimbra <103079756+DaviCoimbra@users.noreply.github.com>
2023-04-04 17:56:02 -0300

    Update Usuario.java

--- avaliacao-online/src/main/java/com/onlinexam/avaliacaoonline/Usuario.java
+++ avaliacao-online/src/main/java/com/onlinexam/avaliacaoonline/Usuario.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class  Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private long id;
     private String email;
