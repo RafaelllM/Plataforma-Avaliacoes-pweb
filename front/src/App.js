7c6c838e967f30a1ca5510e82161a015b234f982
DaviCoimbra <103079756+DaviCoimbra@users.noreply.github.com>
2023-04-25 16:58:35 -0300

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import Cadastro from './pages/Cadastro';
-import Docente from './pages/Docente';
-import Turma from './pages/Turma';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import Cadastro from "./pages/Cadastro";
+import Docente from "./pages/Docente";
+import Turma from "./pages/Turma";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Cadastro/>} />
-        <Route exact path='/Docente' element={<Docente/>} />
-        <Route exact path='/Turma' element={<Turma/>} />
+        <Route exact path="/" element={<Cadastro />} />
+        <Route exact path="/Docente" element={<Docente />} />
+        <Route exact path="/Turma" element={<Turma />} />
+        <Route exact path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
