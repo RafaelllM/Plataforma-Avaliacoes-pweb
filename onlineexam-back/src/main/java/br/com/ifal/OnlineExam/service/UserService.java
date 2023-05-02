870517718dcda54e35d4577c4f0daa69748206b6
DaviCoimbra <103079756+DaviCoimbra@users.noreply.github.com>
2023-05-02 18:56:22 -0300

    Update UserService.java

--- onlineexam-back/src/main/java/br/com/ifal/OnlineExam/service/UserService.java
+++ onlineexam-back/src/main/java/br/com/ifal/OnlineExam/service/UserService.java
@@ -7,7 +7,9 @@ import br.com.ifal.OnlineExam.model.User;
 public interface UserService {
 
     public User SalvarUsuario(User user);
-
+    
+    public String EditarUsuario(User aluno);
+    
     public boolean VerificarEmail(String email);
 
     public List<User> getAllStudents();
