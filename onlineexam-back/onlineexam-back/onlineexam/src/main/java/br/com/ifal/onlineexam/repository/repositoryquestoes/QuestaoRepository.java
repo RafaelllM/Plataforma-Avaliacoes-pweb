9373d4080dc4acc5c7578ff4b2a41e65c4e41e49
Rafael dos Santos Miguel <106350860+RafaelllM@users.noreply.github.com>
2023-05-26 18:32:46 -0300

    Adicionei no lugar errado

--- onlineexam-back/onlineexam-back/onlineexam/src/main/java/br/com/ifal/onlineexam/repository/repositoryquestoes/QuestaoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ifal.onlineexam.repository.repositoryquestoes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.ifal.onlineexam.model.questoes.Questao;
-
-public interface QuestaoRepository extends JpaRepository<Questao, Long> {
-
-}
