dc93f924552acc6d8adcf01e03d55d9b9b363de3
Rafael dos Santos Miguel <106350860+RafaelllM@users.noreply.github.com>
2023-06-12 10:21:17 -0300

    Delete onlineexam-back/onlineexam directory

--- onlineexam-back/onlineexam/src/main/java/br/com/ifal/onlineexam/repository/repositoryquestoes/ProvaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.ifal.onlineexam.repository.repositoryquestoes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.ifal.onlineexam.model.questoes.Prova;
-
-public interface ProvaRepository extends JpaRepository<Prova, Long> {
-    
-}
--- onlineexam-back/onlineexam/src/main/java/br/com/ifal/onlineexam/repository/repositoryquestoes/QuestaoAbertaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.ifal.onlineexam.repository.repositoryquestoes;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.ifal.onlineexam.model.questoes.QuestaoAberta;
-
-public interface QuestaoAbertaRepository extends JpaRepository<QuestaoAberta, Long> {
-
-}
